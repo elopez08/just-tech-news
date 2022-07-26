d02f31232a5a4589aaa994443d087c6beff0b277
ejlopez08 <edgardoca2007@gmail.com>
2022-07-26 11:14:57 -0700

    Adding the files from 13.1 and putting it in the system to track

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to our db
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: '',
+      dialect: 'mysql',
+      password: '',
+      port: 3306,
+    }
+  );
+
+module.exports = sequelize;
